d868f8426bab601e859954b429558b682386e20d
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-16 13:44:08 -0700

    finish configurating app, implement mvc pattern

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+// Global middlewares
+app.use((req, res, next) => {
+  req.time = new Date().toISOString();
+  next();
+});
+app.use(morgan('dev'));
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log('Hello from middleware!');
+  next();
+});
+
+// Mount routers
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
---
+++ controllers/tourController.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+// Route handlers
+exports.getAllTours = (req, res) => {
+  console.log(req.time);
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.time,
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+exports.getTour = (req, res) => {
+  const tour = tours.find((t) => t.id === parseInt(req.params.id));
+  if (!tour) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: { tour },
+  });
+};
+
+exports.createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = { id: newId, ...req.body };
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: { tour: newTour },
+      });
+    }
+  );
+};
+
+exports.updateTour = (req, res) => {
+  const tour = tours.find((t) => t.id === parseInt(req.params.id));
+  if (!tour) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+  // Patch
+  res.status(200).json({ status: 'success', data: '...' });
+};
+
+exports.deleteTour = (req, res) => {
+  const tour = tours.find((t) => t.id === parseInt(req.params.id));
+  if (!tour) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+  // Delete
+  res.status(204).json({ status: 'success', data: null });
+};
