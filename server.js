c336c1efde884a572066933e6b6d1aab3adc4672
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-16 11:33:13 -0700

    setup express

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ success: true });
-});
+const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours-simple.json'));
 
-app.post('/', (req, res) => {
-  res.json({ success: 'posted' });
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const PORT = process.env.PORT || 5000;
