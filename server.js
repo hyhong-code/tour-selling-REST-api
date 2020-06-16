a23b3bdaadc3f2e1ee5dd1b74278621f5228cf41
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-16 15:15:12 -0700

    connect to mongodb atlas

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 require('dotenv').config({ path: './config.env' });
+const mongoose = require('mongoose');
 const app = require('./app');
 
-// Start server
+mongoose
+  .connect(process.env.MONGO_DB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(console.log(`MongoDB connected`));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server up on port ${PORT}`));
