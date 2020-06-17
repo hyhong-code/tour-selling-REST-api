9d5059264bc39999c9739326532f1d9c6053f1c0
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-17 12:37:49 -0700

    refactor server.js

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
-require('dotenv').config({ path: './config.env' });
-const mongoose = require('mongoose');
+require('dotenv').config({ path: './config/config.env' });
+const connectDB = require('./config/db');
+// Prevent uncaughtException globally
+process.on('uncaughtException', (err) => {
+  console.log(`Caught uncaught exception! server shutting down...`);
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 const app = require('./app');
 
-mongoose
-  .connect(process.env.MONGO_DB_CONNECTION_STRING, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(console.log(`MongoDB connected`));
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(
   PORT,
-  console.log(`Server up on port ${PORT} in ${process.env.NODE_ENV} mode`)
+  console.log(`Server up on port ${PORT} in ${process.env.NODE_ENV} mode...`)
 );
 
 // Prevent unhandledRejection globally
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
   console.log(`Caught unhandled rejection! server shutting down...`);
+  console.log(err.name, err.message);
 
   // Let server finish handling pending reqs then shut down.
   server.close(() => process.exit(1)); // Exit on unhandled rejection use code 1
