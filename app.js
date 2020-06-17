eaaf9e6de5ccc2e66ccb89efd7c6626e4a60cf60
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-16 20:11:46 -0700

    add getMonthlyPlan route that uses aggregation pipeline to project if certain month is busy

--- app.js
+++ app.js
@@ -16,10 +16,6 @@ if (process.env.NODE_ENV === 'development') {
 }
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-app.use((req, res, next) => {
-  console.log('Hello from middleware!');
-  next();
-});
 
 // Mount routers
 app.use('/api/v1/tours', tourRouter);
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -85,7 +85,7 @@ exports.deleteTour = async (req, res) => {
   }
 };
 
-exports.getTourStats = async (req, res, next) => {
+exports.getTourStats = async (req, res) => {
   try {
     const stats = await Tour.aggregate([
       { $match: { ratingsAverage: { $gte: 4.5 } } },
@@ -104,6 +104,54 @@ exports.getTourStats = async (req, res, next) => {
       // { $match: { _id: { $ne: 'EASY' } } },
     ]);
     res.status(200).json({ status: 'success', data: { stats } });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ status: 'fail', message: 'Invalid data sent!' });
+  }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = parseInt(req.params.year, 10);
+    const plan = await Tour.aggregate([
+      { $unwind: '$startDates' },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: {
+          month: '$_id',
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: {
+          numTourStarts: -1,
+        },
+      },
+      {
+        $limit: 12,
+      },
+    ]);
+    res
+      .status(200)
+      .json({ status: 'success', results: plan.length, data: { plan } });
   } catch (error) {
     console.log(error);
     res.status(400).json({ status: 'fail', message: 'Invalid data sent!' });
