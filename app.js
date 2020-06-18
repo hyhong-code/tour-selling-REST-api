b4829f81716f283ec6b8051e52dcc300dece6c57
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-18 10:52:47 -0700

    implement rate limiting, set secure HTTP headers, sanitize data, prevent http params polution

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,16 +13,49 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// Global middlewares
+// Global middlewares:
+
+// Set security HTTP headers
+app.use(helmet());
+// Dev logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+// Rate limiting
+const limiter = rateLimit({
+  // 100 requests from same ip in one hour
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'To many requests from this IP, try again in an hour',
+});
+app.use('/api', limiter);
+// Body parser
+app.use(express.json({ limit: '10kb' }));
+// Data sanitization against NoSQL injection
+app.use(mongoSanitize());
+// Data sanitization against XSS attack
+app.use(xss());
+// Prevent http params polution (clears up query string)
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+// Serve static files
+app.use(express.static(`${__dirname}/public`));
+// For testing
 if (process.env.NODE_ENV === 'development') {
   app.use((req, res, next) => {
     req.time = new Date().toISOString();
     next();
   });
-  app.use(morgan('dev'));
 }
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
 
 // Mount routers
 app.use('/api/v1/tours', tourRouter);
